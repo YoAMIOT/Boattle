35a543f9df419693c8c86d99554da80a91477c22
AMIOT Yoann <yoann.amiot.dev@gmail.com>
2022-06-26 15:22:45 +0200

    Server can connect to client

--- Boattle/scripts/MainMenu.gd
+++ Boattle/scripts/MainMenu.gd
@@ -1,20 +1,79 @@
 extends Control
 
+###Variables###
+var regex : RegEx = RegEx.new();
+var connectionSuccess : bool = false;
+
+
+
+func _ready():
+	regex.compile("\\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b");
+	var _signalFailedConnect = Server.connect("failedToConnect", self, "onConnectionFailed");
+	var _signalSuccessConnect = Server.connect("successfullyConnected", self, "onConnectionSuccess");
 
 
 func _on_PlayButton_pressed():
 	get_node("Menu").visible = false;
 	get_node("ServerMenu").visible = true;
 
-
 func _on_QuitButton_pressed():
 	get_tree().quit();
-
-
-func _on_JoinButton_pressed():
-	pass # Replace with function body.
-
 
 func _on_BackButton_pressed():
 	get_node("ServerMenu").visible = false;
 	get_node("Menu").visible = true;
+
+
+
+func _on_JoinButton_pressed():
+	get_node("ServerMenu/Error").visible = false;
+	get_node("ServerMenu/Error/EmptyAddress").visible = false;
+	get_node("ServerMenu/Error/InvalidAddress").visible = false;
+
+	if get_node("ServerMenu/IpAddress").text.empty() == false:
+		var result = regex.search(get_node("ServerMenu/IpAddress").text);
+		if result:
+			Server.ip = get_node("ServerMenu/IpAddress").text;
+			Server.connectToServer();
+			get_node("ServerMenu").visible = false;
+			get_node("Connecting").visible = true;
+			get_node("Connecting/TimeOutTimer").start();
+		else:
+			get_node("ServerMenu/Error").visible = true;
+			get_node("ServerMenu/Error/InvalidAddress").visible = true;
+	elif get_node("ServerMenu/IpAddress").text.empty() == true:
+		get_node("ServerMenu/Error").visible = true;
+		get_node("ServerMenu/Error/EmptyAddress").visible = true;
+
+
+func onConnectionFailed():
+	get_node("Connecting/ConnectingLabel").visible = false;
+	get_node("Connecting/LoadingAnim").visible = false;
+	get_node("Connecting/ConnectionFailedLabel").visible = true;
+	get_node("Connecting/FailureTimer").start();
+
+func onConnectionSuccess():
+	get_node("Connecting/ConnectingLabel").visible = false;
+	get_node("Connecting/LoadingAnim").visible = false;
+	get_node("Connecting/ConnectedLabel").visible = true;
+	connectionSuccess = true;
+	get_node("Connecting/SuccessTimer").start();
+
+func _on_TimeOutTimer_timeout():
+	if connectionSuccess == false:
+		get_node("Connecting/ConnectingLabel").visible = false;
+		get_node("Connecting/LoadingAnim").visible = false;
+		get_node("Connecting/ConnectionFailedLabel").visible = true;
+		get_node("Connecting/FailureTimer").start();
+
+func _on_FailureTimer_timeout():
+	get_node("Connecting/ConnectingLabel").visible = true;
+	get_node("Connecting/LoadingAnim").visible = true;
+	get_node("Connecting/ConnectedLabel").visible = false;
+	get_node("Connecting/ConnectionFailedLabel").visible = false;
+	get_node("Connecting").visible = false;
+	get_node("ServerMenu").visible = true;
+	Server.resetNetworkPeer();
+
+func _on_SuccessTimer_timeout():
+	get_node(".").visible = false;
---
+++ Boattle/scripts/singletons/Server.gd
@@ -0,0 +1,31 @@
+extends Node
+
+var network = null;
+var ip : String = "";
+var port : int = 4180;
+
+signal successfullyConnected;
+signal failedToConnect;
+
+
+
+func connectToServer():
+	network = NetworkedMultiplayerENet.new();
+	network.create_client(ip, port);
+	get_tree().set_network_peer(network);
+	var _signalFailedConnect = network.connect("connection_failed", self, "connectionFailed");
+	var _signalSuccessConnect = network.connect("connection_succeeded", self, "connectionSucceeded");
+
+func connectionFailed():
+	print("! Failed to connect !");
+	emit_signal("failedToConnect");
+
+func connectionSucceeded():
+	print("===Sucessfully connected===");
+	emit_signal("successfullyConnected");
+
+func resetNetworkPeer():
+	if get_tree().has_network_peer():
+		network.disconnect("connection_failed", self, "connectionFailed");
+		network.disconnect("connection_succeeded", self, "connectionSucceeded");
+		get_tree().network_peer = null
