f8c4d0acf13bf72f6f81d6e0b23f57591e411e08
AMIOT Yoann <yoann.amiot.dev@gmail.com>
2022-10-25 22:01:05 +0200

    fix UI script

--- BoattleServer/scripts/UI/UI.cs
+++ BoattleServer/scripts/UI/UI.cs
@@ -20,12 +20,12 @@ public class UI : Control{
         PlayersList = this.GetNode<ItemList>("PlayersTab/PlayersList");
         DataManager = this.GetNode<DataManager>("root/DataManager");
 
-        ServerTabBtn.Connect("toggled", this, "ServerTabBtnToggled", pressed);
-        PlayersTabBtn.Connect("toggled", this, "PlayersTabBtnToggled", pressed);
+        ServerTabBtn.Connect("toggled", this, "ServerTabBtnToggled");
+        PlayersTabBtn.Connect("toggled", this, "PlayersTabBtnToggled");
         KickBtn.Connect("pressed", this, "KickBtnPressed");
         ValidateBtn.Connect("pressed", this, "ValidateBtnPressed");
         CancelBtn.Connect("pressed", this, "CancelBtnPressed");
-        PlayersList.Connect("item_selected", this, "PlayersListItemSelected", index);
+        PlayersList.Connect("item_selected", this, "PlayersListItemSelected");
     }
 
     private void ServerTabBtnToggled(bool pressed){
@@ -47,30 +47,30 @@ public class UI : Control{
     }
 
     public void addPlayerToList(string playerName){
-        if (!PlayerList.Items.Has(playerName)){
-            PlayeList.AddItem(playerName);
+        if (!PlayersList.Items.Contains(playerName)){
+            PlayersList.AddItem(playerName);
         }
     }
 
     public void removePlayerFromList(string playerName){
-        for (int i = 0; i < PlayerList.GetItemCount(); i++){
-            if (PlayerList.GetItemText(i) == playerName){
-                PlayerList.RemoveItem(i);
+        for (int i = 0; i < PlayersList.GetItemCount(); i++){
+            if (PlayersList.GetItemText(i) == playerName){
+                PlayersList.RemoveItem(i);
             }
         }
     }
 
     private void PlayersListItemSelected(int index){
         KickBtn.Disabled = false;
-        private string playerName = PlayerList.GetItemText(index);
-        private int playerId = DataManager.getIdFromName(playerName);
+        string playerName = PlayersList.GetItemText(index);
+        int playerId = DataManager.getIdFromName(playerName);
         selectedPlayerId = playerId;
-        private int generalRange = 384;
-        this.GetNode<Label>("PlayersTab/PlayerNameLabel").Text = "Player name: " + playerName; 
-        this.GetNode<Label>("PlayersTab/PlayerIdLabel").Text = "Connection ID: " + <string>playerId; 
-        this.GetNode<Label>("PlayersTab/PlayerViewRangeLabel").Text = "View range: " + <string>(generalRange * DataManager.shipsDictionary[DataManager.playerShipsStatsDictionary[playerName].ship].viewRange); 
-        this.GetNode<Label>("PlayersTab/PlayerMoveRangeLabel").Text = "Move range: " + <string>(generalRange * DataManager.shipsDictionary[DataManager.playerShipsStatsDictionary[playerName].ship].moveRange); 
-        this.GetNode<Label>("PlayersTab/PlayerShootRangeLabel").Text = "Shoot range: " + <string>(generalRange * DataManager.shipsDictionary[DataManager.playerShipsStatsDictionary[playerName].ship].shootRange);
+        int generalRange = 384;
+        this.GetNode<Label>("PlayersTab/PlayerNameLabel").Text = "Player name: " + playerName;
+        this.GetNode<Label>("PlayersTab/PlayerIdLabel").Text = "Connection ID: " + playerId.ToString();
+        this.GetNode<Label>("PlayersTab/PlayerViewRangeLabel").Text = "View range: " + (string)(generalRange * DataManager.shipsDictionary[DataManager.playerShipsStatsDictionary[playerName].ship].viewRange);
+        this.GetNode<Label>("PlayersTab/PlayerMoveRangeLabel").Text = "Move range: " + (string)(generalRange * DataManager.shipsDictionary[DataManager.playerShipsStatsDictionary[playerName].ship].moveRange);
+        this.GetNode<Label>("PlayersTab/PlayerShootRangeLabel").Text = "Shoot range: " + (string)(generalRange * DataManager.shipsDictionary[DataManager.playerShipsStatsDictionary[playerName].ship].shootRange);
     }
 
     private void KickBtnPressed(){
@@ -78,14 +78,14 @@ public class UI : Control{
     }
 
     private void ValidateBtnPressed(){
-        if (DataManager.connectedPlayersDictionary.Has(selectedPlayerId)){
-            this.GetParent().kickPlayer(selectedPlayerId, this.GetNode<Label>("PlayersTab/KickWindow/reason").text);
+        if (DataManager.connectedPlayersDictionary.Contains(selectedPlayerId)){
+            this.GetParent<Node>().kickPlayer(selectedPlayerId, this.GetNode<LineEdit>("PlayersTab/KickWindow/Reason").Text);
+            CancelBtnPressed();
         }
-        CancelBtnPressed();
     }
-    
+
     private void CancelBtnPressed(){
         this.GetNode<Control>("PlayersTab/KickWindow").Visible = false;
-        this.GetNode<Label>("PlayersTab/KickWindow/reason").text = "";
+        this.GetNode<LineEdit>("PlayersTab/KickWindow/Reason").Text = "";
     }
 }
