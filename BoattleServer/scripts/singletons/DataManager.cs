6121a877779a2cb08d2e77dd865224e7edefbc0f
YoAMIOT <yoann.amiot.dev@gmail.com>
2022-10-26 10:57:27 +0200

    WIP DataManager.cs

--- BoattleServer/scripts/singletons/DataManager.cs
+++ BoattleServer/scripts/singletons/DataManager.cs
@@ -12,4 +12,90 @@ public class DataManager: Node {
     public Godot.Collections.Dictionary playersShipsStatsDictionary;
     public Godot.Collections.Dictionary shipsDictionary;
     public Godot.Collections.Dictionary turnDictionary;
+
+    private override void _Ready(){
+        loadPlayerDatas();
+        loadPlayersPasswords();
+        loadShipsStats();
+        loadPlayersShipsStats();
+    }
+
+
+
+    public void savePlayersDatas(){
+
+    }
+
+    public void loadPlayerDatas(){
+
+    }
+
+    public void saveDatasOfAPlayer(string playerName, Vector2 position){
+
+    }
+
+
+
+    public void savePlayersPasswords(){
+
+    }
+
+    public void loadPlayersPasswords(){
+
+    }
+
+    public void savePasswordForAPlayer(string password, string salt, string playerName){
+
+    }
+
+
+
+    public void savePlayersShipsStats(){
+
+    }
+
+    public void loadPlayersShipsStats(){
+
+    }
+
+    public void saveShipsStatsForAPlayer(string playerName, string ship){
+
+    }
+
+    public void loadShipsStats(){
+
+    }
+    public void createShipStatsForAPlayer(string playerName){
+
+    }
+
+
+
+    public void playerConnected(int playerId, string playerName){
+
+    }
+
+    public void playerDisconnected(int playerId){
+
+    }
+
+    public bool isPlayerConnected(string playerName){
+        private bool connected = false;
+        return connected;
+    }
+
+
+
+    public int getIdFromName(string playerName){
+        private int playerId;
+        return playerId;
+    }
+
+
+
+    public void printError(int error){
+        if (error != 0){
+            GD.Print("Error", error);
+        }
+    }
 }
