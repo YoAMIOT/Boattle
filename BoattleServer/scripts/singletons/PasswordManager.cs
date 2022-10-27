52fe742dc5e54ab44b114778213a66eecfaacddc
AMIOT Yoann <83771527+YoAMIOT@users.noreply.github.com>
2022-10-27 15:22:16 +0200

    WIP PasswordManager.cs

--- BoattleServer/scripts/singletons/PasswordManager.cs
+++ BoattleServer/scripts/singletons/PasswordManager.cs
@@ -1,2 +1,12 @@
 using Godot;
 using System;
+
+public class PasswordManager: Node {
+    Godot.Collections.Dictionary wrongPasswordDictionary = {};
+
+    public void validatePassword(bool registration, string password, string playerName) {}
+
+    private string generateSalt() {}
+
+    private string generateHashedPassword(string password, string salt) {}
+}
