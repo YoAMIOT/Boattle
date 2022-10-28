2718b337d409afc842b5df1b7c2884de5266f640
AMIOT Yoann <83771527+YoAMIOT@users.noreply.github.com>
2022-10-28 14:44:11 +0200

    Done switching PasswordManager to C#

--- BoattleServer/scripts/singletons/PasswordManager.cs
+++ BoattleServer/scripts/singletons/PasswordManager.cs
@@ -6,32 +6,34 @@ public class PasswordManager: Node {
     private DataManager DataManager;
     private Server Server;
 
-    public override void _Ready(){
-        DataManager = this.GetNode<DataManager>("/root/DataManager");
-        Server = this.GetNode<Server>("/root/Server");
+    public override void _Ready() {
+        DataManager = this.GetNode < DataManager > ("/root/DataManager");
+        Server = this.GetNode < Server > ("/root/Server");
     }
 
     public void validatePassword(bool registration, string password, string playerName) {
         int playerId = DataManager.getIdFromName(playerName);
-        if (registration){
+        if (registration) {
             string salt = generateSalt();
             string hashedPassword = generateHashedPassword(password, salt);
             DataManager.savePasswordForAPlayer(hashedPassword, salt, playerName);
             validatePassword(false, password, playerName);
-        } else if (!registration){
+        } else if (!registration) {
             string retrievedSalt = DataManager.playersPasswordsDictionary[playerName].salt;
             string hashedPassword = generateHashedPassword(password, retrievedSalt);
-            if (!hashedPassword == DataManager.playersPasswordsDictionary[playerName].password){
+            if (!hashedPassword == DataManager.playersPasswordsDictionary[playerName].password) {
                 Server.wrongPasswordEntered(playerId);
-                if (wrongPasswordDictionary.Contains(playerName)){
+                if (wrongPasswordDictionary.Contains(playerName)) {
                     wrongPasswordDictionary[playerName] += 1;
-                    if (wrongPasswordDictionary[playerName] == 3){
+                    if (wrongPasswordDictionary[playerName] == 3) {
                         Server.kickPlayer(playerId, "You entered 3 wrong passwords");
                         wrongPasswordDictionary.Remove(playerName);
                     }
                 } else {
                     wrongPasswordDictionary[playerName] = 1;
                 }
+            } else if (hashedPassword == DataManager.playersPasswordsDictionary[playerName].password){
+                Server.logIn(playerId, playerName);
             }
         }
     }
@@ -44,7 +46,7 @@ public class PasswordManager: Node {
 
     private string generateHashedPassword(string password, string salt) {
         int rounds = Math.Pow(2, 18);
-        while (rounds > 0){
+        while (rounds > 0) {
             password = (password + salt).SHA256Text();
             rounds -= 1;
         }
