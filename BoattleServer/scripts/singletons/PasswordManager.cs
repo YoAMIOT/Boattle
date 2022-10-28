ddc7b33c444b189e3867bea623ad2ea0eb7de670
AMIOT Yoann <83771527+YoAMIOT@users.noreply.github.com>
2022-10-28 08:05:26 +0200

    WIP PasswordManager.cs

--- BoattleServer/scripts/singletons/PasswordManager.cs
+++ BoattleServer/scripts/singletons/PasswordManager.cs
@@ -6,13 +6,18 @@ public class PasswordManager: Node {
 
     public void validatePassword(bool registration, string password, string playerName) {}
 
-    private string generateSalt(){
+    private string generateSalt() {
         GD.Randomize();
         string salt = GD.Randi().SHA256Text();
         return salt;
     }
 
     private string generateHashedPassword(string password, string salt) {
-        return "";
+        int rounds = Math.Pow(2, 18);
+        while (rounds > 0){
+            password = (password + salt).SHA256Text();
+            rounds -= 1;
+        }
+        return password;
     }
 }
