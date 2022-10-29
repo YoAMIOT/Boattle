b4d733ddc311e8af805c8868363d325fd1fde963
AMIOT Yoann <83771527+YoAMIOT@users.noreply.github.com>
2022-10-29 08:05:42 +0200

    WIP Server.cs

--- BoattleServer/scripts/Server.cs
+++ BoattleServer/scripts/Server.cs
@@ -1,17 +1,24 @@
 using Godot;
 using System;
 
-public class Server : Node{
-        private NetworkedMultiplayerENet Network = new NetworkedMultiplayerENet();
-        private string ip = "Unknown Address";
-        private int port = 4180;
-        private int maxPlayers = 20;
-        private UPNP UPNP = new UPNP();
-        private bool serverStarted = false;
-        private bool turn = false;
-        private RichTextLabel Log;
-        
-    public override void _Ready(){
-        Log = this.GetNode<RichTextLabel>("Ui/Log");
+public class Server: Node {
+    private NetworkedMultiplayerENet Network = new NetworkedMultiplayerENet();
+    private string ip = "Unknown Address";
+    private int port = 4180;
+    private int maxPlayers = 20;
+    private UPNP UPNP = new UPNP();
+    private bool serverStarted = false;
+    private bool turn = false;
+    private RichTextLabel Log;
+
+    public override void _Ready() {
+        Log = this.GetNode < RichTextLabel > ("Ui/Log");
+        Os.SetWindowFullscreen(false);
+        GetTree().SetAutoAcceptQuit(false);
+        UPNP.Discover(2000, 2, "InternetGatewayDevice");
+        ip = UPNP.QueryExternalAddress();
+        UPNP.AddPortMapping(port, port, "BoattleServer", "UDP");
+        UPNP.AddPortMapping(port, port, "BoattleServer", "TCP");
+        this.GetNode<Label>("UI/IpLabel").Text = ip;
     }
 }
