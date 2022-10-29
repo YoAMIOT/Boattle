22896aa9777eafd13809cdb7e540b090b7409920
AMIOT Yoann <83771527+YoAMIOT@users.noreply.github.com>
2022-10-29 06:57:59 +0200

    WIP Server.cs

--- BoattleServer/scripts/Server.cs
+++ BoattleServer/scripts/Server.cs
@@ -2,8 +2,16 @@ using Godot;
 using System;
 
 public class Server : Node{
-
-    public override void _Ready(){
+        private NetworkedMultiplayerENet Network = new NetworkedMultiplayerENet();
+        private string ip = "Unknown Address";
+        private int port = 4180;
+        private int maxPlayers = 20;
+        private UPNP UPNP = new UPNP();
+        private bool serverStarted = false;
+        private bool turn = false;
+        private RichTextLabel Log;
         
+    public override void _Ready(){
+        Log = this.GetNode<RichTextLabel>("Ui/Log");
     }
 }
