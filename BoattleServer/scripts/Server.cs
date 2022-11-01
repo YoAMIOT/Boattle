04185ae888b9fbcdb1c204627fc24952bd7b5dd3
AMIOT Yoann <83771527+YoAMIOT@users.noreply.github.com>
2022-11-01 07:23:44 +0100

    WIP Server.cs

--- BoattleServer/scripts/Server.cs
+++ BoattleServer/scripts/Server.cs
@@ -15,124 +15,116 @@ public class Server: Node {
 
     public override void _Ready() {
         Log = this.GetNode < Log > ("Ui/Log");
-        DataManager = this.GetNode<DataManager>("/root/DataManager");
-        PasswordManager = this.GetNode<PasswordManager>("/root/PasswordManager");
+        DataManager = this.GetNode < DataManager > ("/root/DataManager");
+        PasswordManager = this.GetNode < PasswordManager > ("/root/PasswordManager");
         OS.WindowFullscreen = false;
         GetTree().SetAutoAcceptQuit(false);
         UPNP.Discover(2000, 2, "InternetGatewayDevice");
         ip = UPNP.QueryExternalAddress();
         UPNP.AddPortMapping(port, port, "BoattleServer", "UDP");
         UPNP.AddPortMapping(port, port, "BoattleServer", "TCP");
-        this.GetNode<Label>("Ui/IpLabel").Text = ip;
-        this.GetNode<Button>("Ui/MaxPlayerMenu/ValidateButton").Connect("pressed", this, "ValidateButtonPressed");
-        this.GetNode<Button>("Ui/StartServer").Connect("pressed", this, "StartServerPressed");
-        this.GetNode<Button>("Ui/Close").Connect("pressed", this, "closeButtonPressed");
-        this.GetNode<Timer>("TurnCooldown").Connect("timeout", this, "turnCooldownTimeOut");
+        this.GetNode < Label > ("Ui/IpLabel").Text = ip;
+        this.GetNode < Button > ("Ui/MaxPlayerMenu/ValidateButton").Connect("pressed", this, "ValidateButtonPressed");
+        this.GetNode < Button > ("Ui/StartServer").Connect("pressed", this, "StartServerPressed");
+        this.GetNode < Button > ("Ui/Close").Connect("pressed", this, "closeButtonPressed");
+        this.GetNode < Timer > ("TurnCooldown").Connect("timeout", this, "turnCooldownTimeOut");
     }
 
     private void ValidateButtonPressed() {
-        maxPlayers = (int) this.GetNode<SpinBox>("Ui/MaxPlayerMenu/Selector").Value + 1;
+        maxPlayers = (int) this.GetNode < SpinBox > ("Ui/MaxPlayerMenu/Selector").Value + 1;
         refreshPlayerCountLabel();
-        this.GetNode<Control>("Ui/MaxPlayerMenu").Visible = false;
-        this.GetNode<Button>("Ui/StartServer").Disabled = false;
+        this.GetNode < Control > ("Ui/MaxPlayerMenu").Visible = false;
+        this.GetNode < Button > ("Ui/StartServer").Disabled = false;
     }
 
     private void StartServerPressed() {
         serverStarted = true;
-        this.GetNode<Button>("Ui/StartServer").Disabled = true;
+        this.GetNode < Button > ("Ui/StartServer").Disabled = true;
         Network.CreateServer(port, maxPlayers);
         GetTree().SetNetworkPeer(Network);
         Network.Connect("peer_connected", this, "PeerConnected");
         Network.Connect("peer_disconnected", this, "PeerDisconnected");
-        this.GetNode<Timer>("TurnCooldown").Start();
+        this.GetNode < Timer > ("TurnCooldown").Start();
     }
-    
+
     private void PeerConnected(int playerId) {
-        if (DataManager.connectedPlayersDictionary.Count == maxPlayers - 1){
+        if (DataManager.connectedPlayersDictionary.Count == maxPlayers - 1) {
             kickPlayer(playerId, "This server is full");
         }
     }
-    
+
     private void PeerDisconnected(int playerId) {
         Log.logPrint("!- " + (string)DataManager.connectedPlayersDictionary[playerId] + " disconnected -!");
         DataManager.playerDisconnected(playerId);
         refreshPlayerCountLabel();
-        if (this.GetNode<Node>("PasswordTimers").HasNode(playerId.ToString())){
-            this.GetNode<Node>("PasswordTimers" + playerId.ToString()).Disconnect("timeout", this, "kickPlayer");
-            this.GetNode<Node>("PasswordTimers" + playerId.ToString()).QueueFree();
+        if (this.GetNode < Node > ("PasswordTimers").HasNode(playerId.ToString())) {
+            this.GetNode < Node > ("PasswordTimers" + playerId.ToString()).Disconnect("timeout", this, "kickPlayer");
+            this.GetNode < Node > ("PasswordTimers" + playerId.ToString()).QueueFree();
         }
         RpcId(0, "killPuppet", playerId);
     }
-    
+
     [Remote]
-    public void newConnectionEstablished(string playerName, int playerId){
+    public void newConnectionEstablished(string playerName, int playerId) {
         bool registration = false;
         Log.logPrint("!- " + playerName + " connected -!");
-        if (!DataManager.playersPasswordsDictionary.Contains(playerName)){
+        if (!DataManager.playersPasswordsDictionary.Contains(playerName)) {
             Vector2 position = Vector2.Zero;
             DataManager.saveDatasOfAPlayer(playerName, position);
             DataManager.createShipStatsForAPlayer(playerName);
             registration = true;
         }
         bool hasPlayerConnected = DataManager.isPlayerConnected(playerName);
-        if (hasPlayerConnected){
+        if (hasPlayerConnected) {
             kickPlayer(playerId, "Player already connected");
-        } else if(!hasPlayerConnected){
+        } else if (!hasPlayerConnected) {
             DataManager.playerConnected(playerId, playerName);
             refreshPlayerCountLabel();
             RpcId(playerId, "authentication", registration);
             Timer TimeOutTimer = new Timer();
-            this.GetNode<Node>("PasswordTimers").AddChild(TimeOutTimer);
+            this.GetNode < Node > ("PasswordTimers").AddChild(TimeOutTimer);
             TimeOutTimer.Name = playerId.ToString();
             TimeOutTimer.WaitTime = 60;
             TimeOutTimer.OneShot = true;
             Godot.Collections.Array arr = new Godot.Collections.Array();
             arr.Add(playerId);
-            TimeOutTimer.Connect("timeout", this, "kickForTimeOut", arr);
+            TimeOutTimer.Connect("timeout", this, "kickPlayer", arr);
             TimeOutTimer.Start();
         }
     }
 
     [Remote]
-    public void receivePasswordValidationRequest(bool registration, string password, string playerName){
+    public void receivePasswordValidationRequest(bool registration, string password, string playerName) {
         PasswordManager.validatePassword(registration, password, playerName);
     }
 
-    public void logIn(int playerId, string playerName){
-
+    public void logIn(int playerId, string playerName) {
+        this.GetNode < Timer > ("PasswordTimers" + playerId.ToString()).Disconnect("timeout", this, "kickPlayer");
+        Vector2 playerPosition = new Vector2(DataManager.playersDatasDictionary[playerName].posX, DataManager.playersDatasDictionary[playerName].posY);
     }
 
-    public void wrongPasswordEntered(int playerId){
+    public void wrongPasswordEntered(int playerId) {
         RpcId(playerId, "wrongPassword");
     }
 
-    public void kickForTimeOut(int playerId){
-        kickPlayer(playerId, "You did not enter password in time, please retry");
-    }
-    public void kickPlayer(int playerId, string reason){
+    public void kickPlayer(int playerId, string reason = "You did not enter your password in time, please retry.") {}
 
-    }
-
-    private void refreshPlayerCountLabel(){
-        this.GetNode<Label>("Ui/ConnectedPeersLabel").Text = "Connected: " + DataManager.connectedPlayersDictionary.Count.ToString() + "/" + (maxPlayers - 1).ToString();
+    private void refreshPlayerCountLabel() {
+        this.GetNode < Label > ("Ui/ConnectedPeersLabel").Text = "Connected: " + DataManager.connectedPlayersDictionary.Count.ToString() + "/" + (maxPlayers - 1).ToString();
     }
 
-    public override void _Notification(int what){
-        if (what == MainLoop.NotificationWmQuitRequest){
+    public override void _Notification(int what) {
+        if (what == MainLoop.NotificationWmQuitRequest) {
             GetTree().Quit();
         }
     }
 
-    private void closeButtonPressed(){
+    private void closeButtonPressed() {
         _Notification(MainLoop.NotificationWmQuitRequest);
     }
 
     [Remote]
-    private void receiveTurnData(string action, Vector2 position, string playerName = "", double radius = 0.1){
+    private void receiveTurnData(string action, Vector2 position, string playerName = "", double radius = 0.1) {}
 
-    }
-
-    private void turnCooldownTimeOut(){
-
-    }
+    private void turnCooldownTimeOut() {}
 }
